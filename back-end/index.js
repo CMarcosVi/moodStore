92ee5631c68f8c8d480c70bd457a1b9bfe9e8c71
Marcos <127824847+CMarcosVi@users.noreply.github.com>
2025-03-05 11:41:54 -0300

    Commit-update-melhorias

--- back-end/index.js
+++ back-end/index.js
@@ -5,25 +5,6 @@ import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
-import requestUser from './Controllers/Admin/RequestUser.js'
-import requestAllUser from './Controllers/Admin/ResquerAllUsers.js';
-import createUser from './Controllers/Admin/CreateUser.js';
-import deleteUser from './Controllers/Admin/DeletUsar.js';
-import updateUser from './Controllers/Admin/EditUser.js';
-
-
-import createProduct from './Controllers/Products/CreateProducts.js';
-import updateProduct from './Controllers/Products/EditProducts.js';
-import requestAllProduct from './Controllers/Products/RequestAllProducts.js';
-import requestProducts from './Controllers/Products/RequestProduct.js';
-import deleteProduct from './Controllers/Products/DeleteProducts.js';
-
-import getAllTeams from './Controllers/Admin/team/AllTems.js';
-import getTeamById from './Controllers/Admin/team/FindTeam.js';
-import createTeam from './Controllers/Admin/team/CreateTeam.js';
-import deleteTeam from './Controllers/Admin/team/DeleteTeam.js';
-import updateTeam from './Controllers/Admin/team/EditTeam.js';
-
 
 import cookieParser from 'cookie-parser';
 import verificarToken from './Middlewares/auth.js';
@@ -34,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.post('/loginUser', async (req, res) => { // make the route handler async
+app.post('/loginUser', async (req, res) => {
     try {
         const { default: loginUser } = await import('./Controllers/User/LoginUser.js');
         loginUser(req, res);
@@ -43,64 +24,149 @@ app.post('/loginUser', async (req, res) => { // make the route handler async
         res.status(500).send('Internal Server Error');
     }
 });
-
-
-app.post('/admin/FindUser',verificarToken, (req,res,) => {
-    requestUser(req,res)
-})
-
-app.post('/admin/ResquerAllUsers',verificarToken,(req,res) => {
-    requestAllUser(req,res)
-})
-
-app.delete('/admin/DeleteUser',verificarToken, (req, res) => {
-    deleteUser(req, res)
+app.post('/admin/FindUser', verificarToken, async (req, res) => {
+    try {
+        const { default: requestUser } = await import('./Controllers/Admin/RequestUser.js');
+        requestUser(req, res);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
+app.post('/admin/ResquerAllUsers',verificarToken, async (req,res) => {
+    try {
+        const { default: requestAllUser } = await import('./Controllers/Admin/ResquerAllUsers.js');
+        requestAllUser(req, res);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
 
-app.post('/admin/createAcount',verificarToken, (req,res) => {
-    createUser(req,res)
 })
-app.put('/admin/updateUser',verificarToken, (req,res) => {
-    updateUser(req,res)
+app.delete('/admin/DeleteUser',verificarToken, async (req, res) => {
+    try {
+        const { default: deleteUser } = await import('./Controllers/Admin/DeletUsar.js');
+        deleteUser(req, res);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+app.post('/admin/createAcount',verificarToken, async (req,res) => {
+    try {
+        const { default: createUser } = await import('./Controllers/Admin/CreateUser.js');
+        createUser(req, res);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
 })
+app.put('/admin/updateUser',verificarToken, async (req,res) => {
+    try {
+        const { default: updateUser } = await import('./Controllers/Admin/EditUser.js');
+        updateUser(req, res);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
+})
+
 /*
 products
 */
-
-app.post('/products/CreateProduct', (req,res) => {
-    createProduct(req,res)
+app.post('/products/CreateProduct', async (req,res) => {
+    try {
+        const { default: createProduct } = await import('./Controllers/Products/CreateProducts.js');
+        createProduct(req, res);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
 })
-app.put('/products/EditProduct', (req, res) => {
-    updateProduct(req, res);
+app.put('/products/EditProduct',async (req, res) => {
+    try {
+        const { default: updateProduct } = await import('./Controllers/Products/EditProducts.js');
+        updateProduct(req, res);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
-app.post('/products/RequestAllProducts', (req,res) => {
-    requestAllProduct(req,res)
+app.post('/products/RequestAllProducts',async (req,res) => {
+    try {
+        const { default: requestAllProduct } = await import('./Controllers/Products/RequestAllProducts.js');
+        requestAllProduct(req, res);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
 })
-app.post('/products/RequestProduct', (req,res) => {
-    requestProducts(req,res)
+app.post('/products/RequestProduct',async (req,res) => {
+    try {
+        const { default: requestProducts } = await import('./Controllers/Products/RequestProduct.js');
+        requestProducts(req, res);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
 })
-app.delete('/products/DeleteProduct', (req,res) => {
-    deleteProduct(req,res)
+app.delete('/products/DeleteProduct',async (req,res) => {
+    try {
+        const { default: deleteProduct } = await import('./Controllers/Products/DeleteProducts.js');
+        deleteProduct(req, res);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
 })
 
 /*
 Teams
 */
-
-app.post('/admin/teams/AllTeams',verificarToken, (req,res) => {
-    getAllTeams(req,res)
+app.post('/admin/teams/AllTeams',verificarToken, async (req,res) => {
+    try {
+        const { default: getAllTeams } = await import('./Controllers/Admin/team/AllTems.js');
+        getAllTeams(req, res);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
 })
-app.post('/admin/teams/CreateTeam',verificarToken, (req,res) => {
-    createTeam(req,res)
+app.post('/admin/teams/CreateTeam',verificarToken, async (req,res) => {
+    try {
+        const { default: createTeam } = await import('./Controllers/Admin/team/CreateTeam.js');
+        createTeam(req, res);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
 })
-app.post('/admin/teams/FindTeam',verificarToken, (req,res) => {
-    getTeamById(req,res)
+app.post('/admin/teams/FindTeam',verificarToken, async (req,res) => {
+    try {
+        const { default: getTeamById } = await import('./Controllers/Admin/team/FindTeam.js');
+        getTeamById(req, res);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
 })  
-app.delete('/admin/teams/DeleteTeam',verificarToken, (req,res) => {
-    deleteTeam(req,res)
+app.delete('/admin/teams/DeleteTeam',verificarToken, async (req,res) => {
+    try {
+        const { default: deleteTeam } = await import('./Controllers/Admin/team/DeleteTeam.js');
+        deleteTeam(req, res);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
 })
-app.put('/admin/teams/EditTeam',verificarToken, (req,res) => {
-    updateTeam(req,res)
+app.put('/admin/teams/EditTeam',verificarToken, async (req,res) => {
+    try {
+        const { default: updateTeam } = await import('./Controllers/Admin/team/EditTeam.js');
+        updateTeam(req, res);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
 })
 
 
