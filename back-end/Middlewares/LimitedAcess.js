6f6511683e15c0f78e1b791e248e80d0161bdee7
Marcos <127824847+CMarcosVi@users.noreply.github.com>
2025-03-05 22:38:21 -0300

    commit-atualization

--- back-end/Middlewares/LimitedAcess.js
+++ back-end/Middlewares/LimitedAcess.js
@@ -1,27 +1,27 @@
 import { rateLimit } from 'express-rate-limit'
 
 const generalLimiter = rateLimit({
-    windowMs: 5 * 60 * 1000, // 5 minutos
-    max: 10, // Limite de 5 requisições por IP
+    windowMs: 5 * 60 * 1000,
+    max: 10,
     message: 'Too many requests from this IP, please try again later.',
     standardHeaders: true,
     legacyHeaders: false,
 });
 
 const generalLimiterProducts = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutos
-    max: 150, // Limite de 5 requisições
+    windowMs: 15 * 60 * 1000,
+    max: 150,
     message: 'Too many requests from this IP, please try again later.',
     standardHeaders: true,
     legacyHeaders: false,
 });
 
 const generalLimiterAdmin = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutos
-    max: 50, // Limite de 5 requisições
+    windowMs: 15 * 60 * 1000,
+    max: 50,
     message: 'Too many requests from this IP, please try again later.',
     standardHeaders: true,
     legacyHeaders: false,
 });
 
-export default {generalLimiter,generalLimiterProducts,generalLimiterAdmin};
+export {generalLimiter,generalLimiterProducts,generalLimiterAdmin};
--- back-end/Middlewares/auth.js
+++ back-end/Middlewares/auth.js
@@ -1,27 +1,20 @@
-// /middleware/auth.js
-import User from '../Models/Users.js'; // Modelo de usuário
+import User from '../Models/Users.js';
 
 const verificarToken = async (req, res, next) => {
     const token = '952e437ee9d5d365755c2cc360df6f29262041070eb54df5fa9688d570f36ca88ef434e08755b56acb24f6dddae73b30d9bd4d13b0fb3828252f65c2ea89a8bd';  // Obtém o token dos cookies
 
     if (!token) {
-        // Se não houver token, redireciona para a página de login
         return res.redirect('/login');
     }
 
     try {
-        // Busca o usuário associado a esse token
         const user = await User.findOne({ where: { token } });
 
         if (!user) {
-            // Se o usuário não for encontrado, redireciona para a página de login
             return res.redirect('/login');
         }
-
-        // Se o token for válido, coloca o usuário na requisição para uso nas rotas seguintes
         req.user = user;
 
-        // Passa o controle para o próximo middleware ou rota
         next();
     } catch (error) {
         console.error('Erro ao verificar o token:', error);
