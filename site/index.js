8690b65cacb2948691a47925d4a9640c1e5eefbc
asd <127824847+CMarcosVi@users.noreply.github.com>
2025-03-28 19:03:01 -0300

    commit-site-home-atualizado

--- site/index.js
+++ site/index.js
@@ -2,15 +2,58 @@ const CIRCLE_MOUSE = document.querySelector('.circle-pointer-mouse');
 const LINK_NAV_BAR = document.querySelectorAll('.link-nav-bar')
 const VALUE_INPUT_YES = document.querySelector('.value-yes');
 const VALUE_INPUT_NOT = document.querySelector('.value-not');
+const LIST_TECNOLOGIS_SELECT = document.querySelector('.tecnologis-list')
+const TECNOLOGIS_USED = document.querySelectorAll('.tecnologis')
+const SERVICE_LIST = document.querySelectorAll('.service')
+const MOVING_IMG_MOUSE = document.getElementById("Moving");
+
+
+
+TECNOLOGIS_USED.forEach((element) => {
+    element.addEventListener("click", () => {
+        element.classList.toggle('select-tecnologi');
+    });
+    element.addEventListener("mouseover", () => {
+        CIRCLE_MOUSE.style.borderColor = element.classList.contains('select-tecnologi') ? "#000" : "#fff";
+    });
+    element.addEventListener("mouseout", () => {
+        CIRCLE_MOUSE.style.borderColor = "#fff"; 
+    });
+});
+SERVICE_LIST.forEach((element) => {
+    element.addEventListener("click", () => {
+        element.classList.toggle('select-service');
+    });
+    element.addEventListener("mouseover", () => {
+        if(element.classList.contains('select-service')){
+            CIRCLE_MOUSE.style.borderColor = "#000"; 
+        }
+    })
+    element.addEventListener("mouseout", () => {
+        if(element.classList.contains('select-service')){
+            CIRCLE_MOUSE.style.borderColor = "#fff"; 
+        }
+    })
+})
+
+
+document.addEventListener("mousemove", (ev) => {
+    const positionX = (window.innerWidth / -90 - ev.x) / 70;
+    const positionY = (window.innerHeight / 2 - ev.y) / 50; 
+
+    MOVING_IMG_MOUSE.style.transform = `translate(${positionX}px, ${positionY}px)`;
+});
 
 
 VALUE_INPUT_NOT.addEventListener('click', () => {
     VALUE_INPUT_NOT.classList.add('select-value');
     VALUE_INPUT_YES.classList.remove('select-value');
+    LIST_TECNOLOGIS_SELECT.classList.remove("hide-list");;
 })
 VALUE_INPUT_YES.addEventListener('click', () => {
     VALUE_INPUT_NOT.classList.remove('select-value');
     VALUE_INPUT_YES.classList.add('select-value');
+    LIST_TECNOLOGIS_SELECT.classList.add("hide-list");
 })
 
 document.addEventListener('mousemove', (event) => {
@@ -18,8 +61,7 @@ document.addEventListener('mousemove', (event) => {
     CIRCLE_MOUSE.style.left = `${event.pageX}px`;
     CIRCLE_MOUSE.style.top = `${event.pageY}px`;
 });
-
-  /*
+/*
 LINK_NAV_BAR.forEach(element => {
     element.addEventListener('mouseover', () => {
         CIRCLE_MOUSE.style.display = 'none'
